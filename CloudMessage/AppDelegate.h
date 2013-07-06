cc1c446da875d34f3daf069aaf4ba258cf497b19
SoftwareLab <softwarelab@SoftwareLabtekiMac-mini.local>
2013-07-06 12:51:21 +0800

    Initial Commit

---
+++ CloudMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudMessage
+//
+//  Created by SoftwareLab on 13-7-6.
+//  Copyright (c) 2013年 SoftwareLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ CloudMessage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CloudMessage
+//
+//  Created by SoftwareLab on 13-7-6.
+//  Copyright (c) 2013年 SoftwareLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
